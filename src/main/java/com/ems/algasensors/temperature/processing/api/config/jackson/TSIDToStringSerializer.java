1499c6f287469db8d1df6e8650a37c92f2834c91
Kleber Souza <kleber.souza@any.mail.com.br>
2025-12-15 08:11:54 -0300

    Added TSID to String serializer for API response.

---
+++ src/main/java/com/ems/algasensors/temperature/processing/api/config/jackson/TSIDToStringSerializer.java
@@ -0,0 +1,18 @@
+package com.ems.algasensors.temperature.processing.api.config.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import io.hypersistence.tsid.TSID;
+
+import java.io.IOException;
+
+public class TSIDToStringSerializer extends JsonSerializer<TSID> {
+
+    /*This method returns TSID in String format for API response*/
+    @Override
+    public void serialize(TSID value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeString(value.toString());
+    }
+
+}
